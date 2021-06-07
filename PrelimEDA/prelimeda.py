ce6e579ee4ccd9a49f7087a03860785577ffd54e
NNC <71912558+NyssaCornelius@users.noreply.github.com>
2021-06-07 15:14:51 -0400

    ANOVA on Wage, Industry, and Strike Data
    Simple analyses on these data frames.

--- PrelimEDA/prelimeda.py
+++ PrelimEDA/prelimeda.py
@@ -9,13 +9,19 @@ Created on Sun May 30 16:13:39 2021
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import statsmodels
 import pickle
 import plotly.express as px
+from scipy import stats as sts
+from scipy.stats import pearsonr
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
 
 clean_national_data = pd.read_pickle(".\\Cleaned\\clean_national_data.pkl")
 clean_state_data = pd.read_pickle(".\\Cleaned\\clean_state_data.pkl")
 work_stop = pd.read_pickle(".\\PrelimEDA\\work_stop.pkl")
-minwagestate = pd.read_csv("/Users/nyssacornelius/Desktop/COMP4477/FProj/FProj/min_wage_state.csv", usecols=["Year","State","Effective.Minimum.Wage","Effective.Minimum.Wage.2020.Dollars","CPI.Average"])
+# minwagestate = pd.read_pickle(".\\PrelimEDA\\minwagestate.pkl")
+minwagestate = pd.read_csv("/Users/nyssacornelius/Desktop/COMP4477/FProj/FProj/min_wage_state.csv", usecols=["Year", "State", "Federal.Minimum.Wage", "Effective.Minimum.Wage","Effective.Minimum.Wage.2020.Dollars","CPI.Average"])
+
 
 minwagestate = minwagestate[(minwagestate['State']!= 'District of Columbia') &\
                           (minwagestate['State']!= 'U.S. Virgin Islands') &\
@@ -83,23 +89,23 @@ us_state_abbrev = {'Alabama': 'AL', 'Alaska': 'AK',
 minwagestate['StateCode'] = minwagestate['State'].map(us_state_abbrev)                                                             
 
     #Work stoppage:
-work_stop = work_stop.rename(columns={'Days idle, cumulative for this work stoppage[3]': 'TotalDaysIdle', 'Industry code[1]': 'IndustryCode', 'Work stoppage beginning date': 'StartDate', 'Work stoppage ending date': 'EndDate'})
+# work_stop = work_stop.rename(columns={'Days idle, cumulative for this work stoppage[3]': 'TotalDaysIdle', 'Industry code[1]': 'IndustryCode', 'Work stoppage beginning date': 'StartDate', 'Work stoppage ending date': 'EndDate'})
 
-#Remove the weird [4] and make the column an integer data type:
-# work_stop = work_stop.replace('[4]', np.NaN) #NOT NEEDED
-work_stop['TotalDaysIdle'] = pd.to_numeric(work_stop['TotalDaysIdle'], errors='coerce', downcast='integer')
-work_stop['TotalDaysIdle'] = work_stop['TotalDaysIdle'].astype('Int64')
+# #Remove the weird [4] and make the column an integer data type:
+# # work_stop = work_stop.replace('[4]', np.NaN) #NOT NEEDED
+# work_stop['TotalDaysIdle'] = pd.to_numeric(work_stop['TotalDaysIdle'], errors='coerce', downcast='integer')
+# work_stop['TotalDaysIdle'] = work_stop['TotalDaysIdle'].astype('Int64')
 
-#Fix workstop end date:
-work_stop['EndDate'] = pd.to_datetime(work_stop['EndDate'], errors='coerce', format = '%Y-%m-%d')
+# #Fix workstop end date:
+# work_stop['EndDate'] = pd.to_datetime(work_stop['EndDate'], errors='coerce', format = '%Y-%m-%d')
 
-#Column for duration of work stoppage:
-    #Represents number of days
-work_stop['WSDuration'] = (work_stop['EndDate'] - work_stop['StartDate'])/np.timedelta64(1,'D')+1
+# #Column for duration of work stoppage:
+#     #Represents number of days
+# work_stop['WSDuration'] = (work_stop['EndDate'] - work_stop['StartDate'])/np.timedelta64(1,'D')+1
 # work_stop['WSDuration'] = work_stop['WSDuration']+1
 
 #Change states from string to list of strings:
-work_stop['States'] = work_stop['States'].str.split(",")
+# work_stop['States'] = work_stop['States'].str.split(",")
 
 #VISUALIZATIONS:
 #NOTES:
@@ -112,8 +118,6 @@ work_stop['States'] = work_stop['States'].str.split(",")
 
 
 #Work Stoppage state data:
-    #Need to put states into a list instead of a string:
-        # words = text.split(",")
     #Need to quantify data by state using list comprehension as states are in lists:
 #Are these two essentially the same? Neither are normal, left skewed.
 # work_stop.hist(column='WSDuration')
@@ -160,16 +164,16 @@ codes3digit = iCodes.loc[iCodes['NAICS_Code2017'].str.contains('^\d{3}$'), ['NAI
 iCodes['3digit'] = iCodes['NAICS_Code2017'].str.extract(r'(\d{3})')
 
 #Industry code abbreviated for work stop:
-work_stop['iCodeAb'] = work_stop['IndustryCode'].astype(str).str.extract(r'(\d{3})')
+# work_stop['iCodeAb'] = work_stop['IndustryCode'].astype(str).str.extract(r'(\d{3})')
 
-#fill in nans:
-work_stop.loc[work_stop['iCodeAb'].isnull(), ['iCodeAb']] = work_stop.loc[work_stop['iCodeAb'].isnull(), ['IndustryCode']].values
+# #fill in nans:
+# work_stop.loc[work_stop['iCodeAb'].isnull(), ['iCodeAb']] = work_stop.loc[work_stop['iCodeAb'].isnull(), ['IndustryCode']].values
 
-#Copy column:
-work_stop['iTitle'] = work_stop['iCodeAb'].astype(str)
+# #Copy column:
+# work_stop['iTitle'] = work_stop['iCodeAb'].astype(str)
 
-#Get name from industry code:
-work_stop['iTitle'] = work_stop['iTitle'].replace({k:v for k, v in zip(iCodes['NAICS_Code2017'], iCodes['IndustryTitle'])})
+# #Get name from industry code:
+# work_stop['iTitle'] = work_stop['iTitle'].replace({k:v for k, v in zip(iCodes['NAICS_Code2017'], iCodes['IndustryTitle'])})
 
 
 #Industry strikes aggregated by count:
@@ -179,7 +183,7 @@ industryCounts = work_stop['iTitle'].value_counts().reset_index().rename({'index
 otherStates = {k:'Other' for k in ['East Coast States', 'Nationwide', 'Interstate']}
 stateCounts = pd.Series(np.concatenate(work_stop['States'])).str.strip().replace(otherStates)
 stateCounts = pd.Series(np.where(stateCounts == "", None, stateCounts)).value_counts().reset_index().rename({'index': 'State', 0: 'Counts'}, axis = 1)
- #Need to look and see what they mean by nationwide, east coast states, and interstate
+#Need to look and see what they mean by nationwide, east coast states, and interstate
 
 #Last to do is find any significance for above data
 #Next is to match up sm data from BLS (should be open)
@@ -299,7 +303,7 @@ earnState = earnState.reset_index()
 
 #Do the states with more strikes really have higher minimum wage?
 #Higher average weekly earnings? Even considering the difference in number of employees?
-#Do the states with higher minimum wages truly have significantly higher minimum wages?
+#Do the states with higher minimum wages have significantly higher minimum wages?
 #z-test? How would we figure out directionality?
 #Or maybe is there a correlation with number of strikes and minimum wage
 #Correlation between industry and average weekly earnings?
@@ -307,14 +311,101 @@ earnState = earnState.reset_index()
 
 
 
+cormin = minwagestate.corr(method='spearman')
+#years increase eff min in 2020 $ decreases, obviously CPI increases almost 1:1
+#Min wage 2020 $s decreases as years increase
+#Min wage 2020 $s decreases as CPI average increases
+
+corfull = finalfull.corr(method = 'spearman') #not helpful
+
+#chi square on work_stop?
+
+#would Virginia be a reference - adopts min wage
+#stateCounts
+#minwagestate
+
+#One way ANOVA on States Minimum Wage:
+stateminwage = []
+for state in list(minwagestate.State.unique()):
+    stateminwage.append(list(minwagestate.loc[minwagestate['State'] == state,
+                                              'Effective.Minimum.Wage.2020.Dollars']))
+
+fvalue, pvalue = sts.f_oneway(*stateminwage)
+
+m_comp = pairwise_tukeyhsd(endog=minwagestate['Effective.Minimum.Wage.2020.Dollars'], groups=minwagestate['State'],
+                           alpha=0.05)
+print(m_comp.summary())
+
+#Have to look at in text file, too many groups:
+with open('mcomp_stateminwage.txt', 'w') as f:
+        print(m_comp.summary(), file=f)
 
 
+#One way ANOVA on Avg Wage States:
+avgwagestate = []
+for state in list(final_earnings.state_name.unique()):
+    avgwagestate.append(list(final_earnings.loc[final_earnings['state_name'] == state,
+                                              'AvgWeeklyEarnings']))
+
+fvalue_statewage, pvalue_statewage = sts.f_oneway(*avgwagestate)
+
+m_comp_statewage = pairwise_tukeyhsd(endog=final_earnings['AvgWeeklyEarnings'], groups=final_earnings['state_name'],
+                           alpha=0.05)
+
+#Have to look at in text file, too many groups:
+with open('mcomp_statewage.txt', 'w') as f:
+        print(m_comp_statewage.summary(), file=f)
+
+#One way ANOVA for Avg Wage by Industry:
+avgwageindustry = []
+for industry in list(final_earnings.industry_name.unique()):
+    avgwageindustry.append(list(final_earnings.loc[final_earnings['industry_name'] == industry,
+                                              'AvgWeeklyEarnings']))
+
+fvalue_industrywage, pvalue_industrywage = sts.f_oneway(*avgwageindustry)
+
+m_comp_industrywage = pairwise_tukeyhsd(endog=final_earnings['AvgWeeklyEarnings'], groups=final_earnings['industry_name'],
+                           alpha=0.05)
+
+with open('mcomp_industrywage.txt', 'w') as f:
+        print(m_comp_industrywage.summary(), file=f)
+
+#Subtract minwage from effective, if 0 = minwage state
+
+#Mean of 0s and 1s for a state by comparing state min wage
+#state min wage > fed min wage = 1, else 0
+#mean > .4 = 1, else 0 for minwage state
 
 
+#df: states, strikes, minwage or not
+minwagestate['MinWageStatus'] = np.where((minwagestate['Effective.Minimum.Wage']-minwagestate['Federal.Minimum.Wage']) > 0, 1, 0)
+state_status = minwagestate.groupby(['State'])['MinWageStatus'].mean().reset_index()
+state_status['MinWageStatus'] = np.round(state_status['MinWageStatus'], 2)
 
+state_status['MinWageStatus'] = np.where(state_status['MinWageStatus'] >= 0.4, 'GreaterMinWage', 'MinWage')
 
+#Get full names for stateCounts in statestrikes:
+statestrikes = stateCounts.copy()
+statestrikes.State.replace({v:k for k,v in us_state_abbrev.items()}, inplace = True)
 
+#Merge on state_status state, WY and SD are missing, need to be dropped:
+wagestrike = pd.merge(state_status, statestrikes, on = 'State', how = 'left').dropna()
 
+#One way ANOVA for MinWage and Strikes by State:
+minornot = []
+for status in list(wagestrike.MinWageStatus.unique()):
+    minornot.append(list(wagestrike.loc[wagestrike['MinWageStatus'] == status,
+                                              'Counts']))
+
+fvalue_strikes, pvalue_strikes = sts.f_oneway(*minornot)
+
+m_comp_strikes = pairwise_tukeyhsd(endog=wagestrike['Counts'], groups=wagestrike['MinWageStatus'],
+                           alpha=0.05)
+
+print(m_comp_strikes.summary())
+#would reject because not significant
+#however, minimum wage states do have less strikes, but it's not significantly different
+#could still be due to chance
 
 
 
